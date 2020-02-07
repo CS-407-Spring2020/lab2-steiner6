3eaaa7b5bfc32665dcfa23c222c4b1d9df1aa600
Jackson <steiner6@wisc.edu>
2020-02-06 19:04:32 -0600

    Initial Commit

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.lab2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button myButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button) findViewById(R.id.button);
+        myButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        myButton.setText("Nice Job!");
+
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void goToActivity2(String str) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", str);
+        startActivity(intent);
     }
 }
